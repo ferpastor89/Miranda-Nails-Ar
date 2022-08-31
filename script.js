5d8695fbe3abfbf52c5f833ce1ca5362ecd1a71a
Fernando Pastor <fernandoandres.pastor@gmail.com>
2022-08-31 03:28:38 -0300

    primer commit Miranda Nails art

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+/*Cuando hago CLICK .button, .nav TOOGLE 'activo'*/
+const buttonmenu = document.querySelector('.buttonmenu')
+const dropdownmenu__navlist = document.querySelector('.dropdownmenu__navlist')
+
+buttonmenu.addEventListener('click',()=>{
+    dropdownmenu__navlist.classList.toggle('active')
+})
